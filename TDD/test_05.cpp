bd551207490165cda58dbb7c94ca43729e56e263
Seongeun Choi <Seenz.choi@gmail.com>
2023-06-02 12:33:55 +0900

    [feature] check same size

--- TDD/test_05.cpp
+++ TDD/test_05.cpp
@@ -1,9 +1,19 @@
+#include <string>
+#include <vector>
+
 class SimilarityChecker
 {
 public:
-
+	int checkCharacterLines(const std::string& str1, const std::string& str2)
+	{
+		int a = str1.length();
+		int b = str2.length();
+		int gap = a - b;
+		if (gap == 0) return 60;
+		return 0;
+	}
 };
-
+	
 #ifndef GTEST_INCLUDE_GTEST_GTEST_H_
 int main(int argc, char* argv[])
 {
